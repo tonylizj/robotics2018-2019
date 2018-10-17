70a2470466dd4877cb1e491a514b9a9e63839ace
Tony Li <tonyli8101@gmail.com>
2018-10-16 20:35:19 -0400

    Revert "Merge branch 'master' of https://github.com/tonylizj/robotics2018-2019"
    This reverts commit fcfd3c26351e78fa548688cd0e18ef46c3bd8c71, reversing
    changes made to 326e3bf45dbed99d038fffdb3ecbfb2002f7f20e.

--- magicbot-simple/components/drive.py
+++ magicbot-simple/components/drive.py
@@ -24,15 +24,9 @@ class Drive:
         self.started_driving = True
 
     def execute(self):
-<<<<<<< HEAD:magicbot-simple/components/drive.py
         if self.started_driving:
             self.lMotor.set(-1)
             self.rMotor.set(1)
-=======
-        if self.did_something:
-            self.lMotor.set(1)
-            self.rMotor.set(-1)
->>>>>>> feb55d562fb5f40bc5a1277ae515f12b81b52f69:magicbot-simple/components/component2.py
         else:
             self.lMotor.set(0)
             self.rMotor.set(0)
