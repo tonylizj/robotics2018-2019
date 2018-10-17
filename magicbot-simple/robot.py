8aa726fb33df7fabb4637f1d43d93c0617b87422
idonlakyo <daniel.lisq12@gmail.com>
2018-10-16 21:14:03 -0400

    changing to new stuff

--- magicbot-simple/robot.py
+++ magicbot-simple/robot.py
@@ -3,35 +3,34 @@
 import wpilib
 from magicbot import MagicRobot
 
-from components.component1 import Component1
-from components.DriveTrain import DriveTrain
-from autonomous.two_steps import TwoSteps
+from components.drive import Component1
+from components.drive import Drive
+
 
 class MyRobot(MagicRobot):
-    
     #
     # Define components here
     #
-    
+
     component1 = Component1
-    l_drive = DriveTrain
-    r_drive = DriveTrain
-    auto_r_drive = TwoSteps
-    auto_l_drive = TwoSteps
+    drive = Drive
+
     # You can even pass constants to components
     SOME_CONSTANT = 1
-    
+
     def createObjects(self):
         """Initialize all wpilib motors & sensors"""
-        
+
         # TODO: create button example here
-        
-        self.r_drive_motor = wpilib.Talon(1)
-        self.l_drive_motor = wpilib.Talon(2)
-        self.auto_r_drive_motor = wpilib.Talon(1)
-        self.auto_l_drive_motor = wpilib.Talon(2)
+
+        # self.component1_motor = wpilib.Talon(1)
+        # self.some_motor = wpilib.Talon(2)
+
+        self.lMotor = wpilib.Talon(1)
+        self.rMotor = wpilib.Talon(2)
+
         self.joystick = wpilib.Joystick(0)
-    
+
     #
     # No autonomous routine boilerplate required here, anything in the
     # autonomous folder will automatically get added to a list
@@ -40,13 +39,11 @@ class MyRobot(MagicRobot):
     def teleopPeriodic(self):
         """Place code here that does things as a result of operator
            actions"""
-        
-        try:
-            if self.joystick.getY():
-                self.drive.execute(self.joystick.getY())
-        except:
-            self.onException()
 
-    
+        if self.joystick.getTrigger():
+            self.drive.start_driving()
+            self.drive.set_speed(self.joystick.getY)
+
+
 if __name__ == '__main__':
     wpilib.run(MyRobot)
