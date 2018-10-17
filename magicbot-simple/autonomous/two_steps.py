4654e4cb1adf36657aeb79a405a9ebe50c8f7500
idonlakyo <daniel.lisq12@gmail.com>
2018-10-16 21:14:29 -0400

    still changing

--- magicbot-simple/autonomous/two_steps.py
+++ magicbot-simple/autonomous/two_steps.py
@@ -1,23 +1,23 @@
-import wpilib
 from magicbot import AutonomousStateMachine, tunable, timed_state
-             
-from components.DriveTrain import DriveTrain
-                    
-class TwoSteps(AutonomousStateMachine):
 
+from components.drive import Drive
+
+
+class TwoSteps(AutonomousStateMachine):
     MODE_NAME = 'Two Steps'
     DEFAULT = True
-    
-    motor = wpilib.Talon
-    
+
+    drive = Drive
+
     drive_speed = tunable(-1)
 
-    @timed_state(duration=2, next_state='do_something', first=True)
-    def dont_do_something(self):
-        '''This happens first'''
-        pass
+    # @timed_state(duration=2, next_state='start_driving', first=True)
+    # def dont_do_something(self):
+    # '''This happens first'''
+    # pass
 
-    @timed_state(duration=5)
-    def do_something(self):
-        '''This happens second'''
-        self.drive.do_something()
+    @timed_state(duration=5, first=True)
+    def drive_forward(self):
+        # '''This happens second'''
+        self.drive.start_driving()
+
